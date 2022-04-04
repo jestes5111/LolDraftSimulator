fcb8beec0d314ae75f4ea92ac42f695224ebc747
Jesse Estes <jestes5111@gmail.com>
2022-04-04 10:34:13 -0700

    Added module level dunders, cleaned up code to make PyLint happy

--- lol_draft_simulator.py
+++ lol_draft_simulator.py
@@ -1,25 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """lol_draft_simulator.py: Use Neural Networks to simulate a
    League of Legends draft.
 """
 
+__file__ = 'lol_draft_simulator.py'
+__author__ = 'Jesse Estes'
+__copyright__ = 'Copyright 2022, LolDraftSimulator'
+__credits__ = ['Jesse Estes']
+__license__ = 'MIT'
+__version__ = '1.0.0'
+__maintainer__ = 'Jesse Estes'
+__email__ = 'jestes5111@gmail.com'
+__status__ = 'Prototype'
+
 # --------------------------------------------------------------------------- #
 #                                  Imports                                    #
 # --------------------------------------------------------------------------- #
+# Standard libraries
+
+# Third-party libraries
 import pandas as pd
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
 from sklearn.model_selection import train_test_split
 
+# Local libraries
+
 # --------------------------------------------------------------------------- #
 #                                    Code                                     #
 # --------------------------------------------------------------------------- #
-def classify_picks_bans(data, phase):
-    """Train a classifier to determine what champions are picked and banned.
+def classify_picks_bans(data: np.array, phase: str) -> None:
+    """Use a Neural Network to classify champions selected in the given phase.
 
     Args:
-        phase (string): The given phase of the draft (ex: 'Blue Ban 1').
+        data: Champion selections and their selection phases
+        phase: Draft phase that the champion was selected in
     """
     # Determine what phase is being classified
     target = data[phase]
@@ -28,11 +46,11 @@ def classify_picks_bans(data, phase):
     data_encoder = OrdinalEncoder().fit(data)
     target_encoder = LabelEncoder().fit(target)
 
-    # Transform the data to make it readable by the Neural Network 
+    # Transform the data to make it readable by the Neural Network
     encoded_data = data_encoder.transform(data)
     encoded_target = target_encoder.transform(target)
 
-    # Split the data using an 80/20 split
+    # Split 80% of the data into training data and 20% into testing data
     features_train, features_test, labels_train, labels_test = train_test_split(
         encoded_data, encoded_target, test_size = 0.2
     )
@@ -41,16 +59,16 @@ def classify_picks_bans(data, phase):
     del labels_test
 
     # Create and train a Neural Network classifier
-    classifier = MLPClassifier(max_iter = 1500)
+    classifier = MLPClassifier(max_iter=1500)
     classifier.fit(features_train, labels_train)
-    
-    # Predict the champion selected in the given phase (i.e., picked or banned)
+
+    # Determine the champion selected in the given phase (i.e., picked or banned)
     predictions = classifier.predict(features_test)
 
-    # Revert to the original form of data (champion name)
+    # Un-encode the data (i.e., revert it to champion name)
     predictions = target_encoder.inverse_transform(predictions)
 
-    # Remove duplicates from the list of predictions
+    # Remove duplicates
     predictions = list(set(predictions))
 
     # Check if the selected champion has already been selected
@@ -70,8 +88,12 @@ def classify_picks_bans(data, phase):
             # Exit the loop since the selection has been made
             break
 
-def simulate_draft(data):
-    """Simulate the picks and bans."""
+def simulate_draft(data: np.array) -> None:
+    """Simulate the picks and bans.
+
+    Args:
+        data: Champion selections and their selection phases
+    """
     # Classify the picks and bans of every phase
     # Ban phase one - each team takes turns banning champions
     classify_picks_bans(data, 'Blue Ban 1')
@@ -101,8 +123,12 @@ def simulate_draft(data):
     classify_picks_bans(data, 'Blue Pick 5')
     classify_picks_bans(data, 'Red Pick 5')
 
-def print_draft(selections):
-    """Print the results of the draft in an easy-to-read format."""
+def print_draft(selections: list) -> None:
+    """Print the results of the draft in an easy-to-read format.
+
+    Args:
+        selections: List of champion selections
+    """
     # Inform the user that the simulation
     print('\nDraft simulation finished! Results:\n')
 
@@ -114,7 +140,7 @@ def print_draft(selections):
     print('\tRed Ban 2:\t', selections[3])
     print('\tBlue Ban 3:\t', selections[4])
     print('\tRed Ban 3:\t', selections[5])
-    
+
     print('\nPick phase one - each team picks three champions, snake-style')
     print('\tBlue Pick 1:\t', selections[6])
     print('\tRed Pick 1:\t', selections[7])
@@ -122,13 +148,13 @@ def print_draft(selections):
     print('\tBlue Pick 2:\t', selections[9])
     print('\tBlue Pick 3:\t', selections[10])
     print('\tRed Pick 3:\t', selections[11])
-    
+
     print('\nBan phase two - each team takes turns banning champions')
     print('\tRed Ban 4:\t', selections[12])
     print('\tBlue Ban 4:\t', selections[13])
     print('\tRed Ban 5:\t', selections[14])
     print('\tBlue Ban 5:\t', selections[15])
-    
+
     print('\nPick phase two - each team picks two champions, snake-style')
     print('\tRed Pick 4:\t', selections[16])
     print('\tBlue Pick 4:\t', selections[17])
